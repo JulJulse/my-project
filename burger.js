a00f84fdfee2668da96c9d5f6d306ac49a2ab412
JulJulse <152330795+JulJulse@users.noreply.github.com>
2024-03-23 07:53:11 +0300

    Add files via upload

--- burger.js
+++ burger.js
@@ -1,17 +1,6 @@
-let burger - document.querySelector('.header__burger-btn');
-let overlay - document.querySelector('.overlay');
-let body - document.querySelector('body');
 
-let links - document.querySelector('.menu__link'); [1,2,3,4]
 
-links.forEach(function(element){
-    element.addEventListener('click', toggleMenu);
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("burger").addEventListener("click", function (){
+        document.querySelector("overlay").classList.toggle.apply("overlay")
 })
-
-function toggleMenu(){
-    burger.classList.toggle('header__burger-btn--active');
-    overlay.classList.toggle('overlay--active');
-    body.classList.toggle('.body--active--menu');
-}
-
-burger.addEventListener('click', toggleMenu);
